7d309461754b3ac44e9ec37a2667cf23d5efd3ac
matt <matt@localhost>
2017-11-23 23:48:11 +0100

    Modification de la classe Edge
    On n'arrive toujours pas à créer et ajouter des arrêtes au graph.

--- src/modele/Edge.java
+++ src/modele/Edge.java
@@ -3,35 +3,41 @@ import org.jgrapht.graph.DefaultEdge;
 
 @SuppressWarnings("serial")
 public class Edge extends DefaultEdge implements Comparable<Edge> {
+	private enum DoorType {
+		OPENED,
+		CLOSED,
+		NONE
+	}
 	/**
 	 * Permet de déterminer le type de porte (à retravailler suivant l'implémentation)
 	 */
-	private int doorType;
+	private DoorType doorType;
 	
-	public Edge(int doorType) {
+	public Edge() {
 		super();
-		this.setDoorType(doorType);
+		setDoorType(DoorType.NONE);
 	}
 	
 	@Override
 	public int compareTo(Edge o) {
 		int diffSource = ((Vertex)this.getSource()).compareTo((Vertex)o.getSource());
-		if (diffSource !=0)
+		if (diffSource != 0)
 			return diffSource;
 		else
 			return  ((Vertex)this.getTarget()).compareTo((Vertex)o.getTarget());
 	}
 	
 	public String toString() {
-		String s = "[" + this.getSource().toString() + "<->" + this.getTarget().toString() + "]";
+		String s = "(" + this.getSource().toString() + "<->" 
+					+ this.getTarget().toString() + ")";
 		return s;
 	}
 
-	public int getDoorType() {
+	public DoorType getDoorType() {
 		return doorType;
 	}
 
-	public void setDoorType(int doorType) {
+	private void setDoorType(DoorType doorType) {
 		this.doorType = doorType;
 	}	
 }
