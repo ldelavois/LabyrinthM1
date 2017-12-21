50cc69c57f681d99b64b8b1492cf9d47aa349281
amahazoasy <amahazoasy@breguet.emi.u-bordeaux.fr>
2017-12-21 21:46:11 +0100

    Début tests unitaires

---
+++ src/modele/VertexTest.java
@@ -0,0 +1,78 @@
+package modele;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class VertexTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println("Vertex: avant tout");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		System.out.println("Vertex: après tout");
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("Vertex: avant un test");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("Vertex: après un test");
+	}
+
+	@Test
+	public void testGetX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAreNeighbors() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInBorders() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNbr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNbr() {
+		fail("Not yet implemented");
+	}
+
+}
