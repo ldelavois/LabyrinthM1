917ab73460586cc1c58cad5bc32a5fa5d50633c5
flmatt <matt.p24610@gmail.com>
2017-12-22 20:18:01 +0100

    Documentation de la classe Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,10 +5,19 @@ import controller.Controller;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * Classe permettant de lancer l'application javaFX
+ * @author Ludovic, Julien, Andy, Matthias
+ *
+ */
 public class Main extends Application {
 
 	private static Controller controller;
-
+	
+	/**
+	 * Méthode principale de l'application
+	 * @param args Arguments
+	 */
 	public static void main(String[] args) {
 		controller = Controller.getInstance();
 		launch(args);
